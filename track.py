0f48062d56db5c04bc5a4cdcc2e83cca1ae7bb0d
Jeff <jazzwithjeff@gmail.com>
2017-03-06 23:08:36 -0800

    Support heatmap tracking across frames.

--- track.py
+++ track.py
@@ -1,20 +1,50 @@
+from collections import deque
 import moviepy
 from moviepy.editor import VideoFileClip
+import numpy as np
+from scipy.ndimage.measurements import label
+
 import search as sr
 import train as tr
 
+class Frame:
+    def __init__(self, heatmap, label_boxes):
+        self.heatmap = heatmap
+        self.label_boxes = label_boxes
+        
 class Tracker:
-    def __init__(self, searcher, window_size, search_params):
+    def __init__(self, searcher, search_params, window_size, heatmap_threshold_per_frame):
         self.searcher = searcher
-        self.window_size = window_size
         self.search_params = search_params
+        self.window_size = window_size
+        self.heatmap_threshold_per_frame = heatmap_threshold_per_frame
+        self.frames = deque()
         
     def track(self, img):
-        ### TODO: Implement tracking.
-        heatmap, label_boxes, window_count = searcher.search(img, 'full', self.search_params)
+        self.add_frame(searcher.search(img, 'full', self.search_params))
+        
+        heatmap, label_boxes = self.smooth()
         labeled_img = sr.draw_boxes(img, label_boxes)
         return labeled_img
+        
+    def add_frame(self, search_result):
+        heatmap = search_result[0]
+        label_boxes = search_result[1]
+        self.frames.appendleft(Frame(heatmap, label_boxes))
+        
+        if len(self.frames) > self.window_size:
+            discard_frame = self.frames.pop()
 
+    def smooth(self):
+        heatmaps = []
+        for f in self.frames:
+            heatmaps.append(f.heatmap)
+        heatmap = np.sum(heatmaps, axis = 0)
+        heatmap = sr.apply_threshold(heatmap, self.heatmap_threshold_per_frame * len(self.frames))
+        labels = label(heatmap)
+        label_boxes = sr.convert_to_bboxes(labels)
+        return heatmap, label_boxes
+            
 def process_img(img):
     global tracker
     return tracker.track(img)
@@ -34,7 +64,13 @@ if __name__ == '__main__':
     searcher = sr.Searcher(fp, clf, X_scaler)    
     
     sp = sr.SearchParams.get_defaults()
-    tracker = Tracker(searcher, 5, sp)
     
-    process_video('test_video.mp4', 'output_video/test_video.mp4')
-    #process_video('project_video.mp4', 'output_video/project_video.mp4')
+    for threshold in [1, 2, 3]:
+        print('Threshold: ', threshold)
+        tracker = Tracker(searcher, sp, 5, heatmap_threshold_per_frame = threshold)
+        process_video('test_video.mp4', 'output_video/test_video_th{}.mp4'.format(threshold))
+    
+    for threshold in [1, 2, 3]:
+        print('Threshold: ', threshold)    
+        tracker = Tracker(searcher, sp, 5, heatmap_threshold_per_frame = threshold)
+        process_video('project_video.mp4', 'output_video/project_video_th{}.mp4'.format(threshold))
